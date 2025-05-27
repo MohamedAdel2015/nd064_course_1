ca52209245c3a1248bfd09cfd379ff299d05f648
Mohamed Adel Abdelfattah <maabdelfattah@ejada.com>
2025-05-27 14:42:22 +0200

    Latest Version before CI/CD

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,34 @@
+from flask import json
 from flask import Flask
+import logging
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    app.logger.info('Main request successfull')
     return "Hello World!"
 
+@app.route('/status')
+def status():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.info('Calling Health Check API')
+    return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.info('Calling Metrics Check API')
+    return response
+
 if __name__ == "__main__":
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
+    
     app.run(host='0.0.0.0')
